918f05636fc02720262911536a4c1cf531e9be9a
githublizhipan <738375827@qq.com>
2020-02-26 12:41:15 +0800

    my_code

---
+++ 1.my_code/4.c
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: 4.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月26日 星期三 12时37分25秒
+ ************************************************************************/
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
+    int *num = (int *)malloc(sizeof(int) * (nums1Size + nums2Size));
+    int i = 0, j = 0, k = 0;
+    while(i < nums1Size || j < nums2Size) {
+        if(i < nums1Size && (j >= nums2Size || nums1[i] < nums2[j])) {
+            num[k++] = nums1[i++];
+        } else num[k++] = nums2[j++];
+    }
+    int l = nums1Size + nums2Size;
+    if(l & 1) {
+        return num[l >> 1] * 1.0;
+    } else {
+        return (num[l >> 1] + num[(l >> 1) - 1]) / 2.0;
+    }
+}
+
