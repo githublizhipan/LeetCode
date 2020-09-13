78f97676ff160f24dc8dcd5a0b3e71b675baf700
“lizhipan” <738375827@qq.com>
2020-09-13 16:26:23 +0800

    n分

--- HZOJ/83.cpp
+++ HZOJ/83.cpp
@@ -4,39 +4,26 @@
 	> Mail: 
 	> Created Time: 2020年03月14日 星期六 08时51分40秒
  ************************************************************************/
-
 #include<iostream>
 using namespace std;
+int n, m;
 
-int num[10] = {0, 1, 3, 9, 27, 81, 243, 729};
-char s[2000][2000];
-
-void func(int x, int y, int n) {
-    if(n == 1) {
-        s[x][y] = 'X';
-        return ;
+int func(int s, int left_num, int left_cnt) {
+    if(left_cnt == 0) {
+        if(left_num == 0) return 1;
+        return 0;
     }
-    func(x, y, n - 1);
-    func(x + num[n] / 3 * 2, y, n - 1);
-    func(x + num[n] / 3, y + num[n] /3, n - 1);
-    func(x, y + num[n] / 3 * 2, n - 1);
-    func(x + num[n] / 3 * 2, y + num[n] / 3 * 2, n - 1);
-    return ;
+    int ans = 0;
+    for(int i = s; i <= left_num; i++) {
+        ans += func(i, left_num - i, left_cnt - 1);
+    }
+    return ans;
 }
+
 
 
 int main() {
-    func(1, 1, 7);
-    int n;
-    while(cin >> n, n != -1) {
-        for(int i = 1; i <= num[n]; i++) {
-            for(int j = 1; j <= num[n]; j++) {
-                if(s[i][j] == 'X') cout << 'X';
-                else cout << " ";
-            }
-            cout << endl;
-        }
-        cout << '-' << endl;
-    }    
+    cin >> n >> m;
+    cout << func(1, n, m) << endl;
     return 0;
-}
+} 
